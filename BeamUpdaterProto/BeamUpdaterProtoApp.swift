666bfc1d1a81e7289a497a269937abc83f5dc54f
Ludovic Ollagnier <ludovic@ollagnier.me>
2021-05-03 15:41:44 +0200

    Initial Commit

---
+++ BeamUpdaterProto/BeamUpdaterProtoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeamUpdaterProtoApp.swift
+//  BeamUpdaterProto
+//
+//  Created by Ludovic Ollagnier on 03/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct BeamUpdaterProtoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeamUpdaterProto/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BeamUpdaterProto
+//
+//  Created by Ludovic Ollagnier on 03/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
