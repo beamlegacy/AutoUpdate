f8654df00771d106a74b97b706044b91c564a56b
Ludovic Ollagnier <ludovic@ollagnier.me>
2021-05-06 10:21:25 +0200

    [feature] Unzip and unquaratine

--- UpdateInstaller/UpdateInstaller.swift
+++ UpdateInstaller/UpdateInstaller.swift
@@ -8,9 +8,60 @@
 import Foundation
 
 class UpdateInstaller: UpdateInstallerProtocol {
-    
-    func upperCaseString(_ string: String, withReply reply: @escaping (String) -> Void) {
-        let response = string.uppercased()
-                reply(response)
+
+    let fileManager = FileManager.default
+
+    func installUpdate(archiveURL: URL, binaryToReplace: URL, reply: @escaping (String) -> Void) {
+
+        do {
+            let appURL = try unarchiveZip(at: archiveURL)
+            print(appURL)
+            try unquarantineApp(at: appURL)
+        } catch {
+            reply(error.localizedDescription)
+        }
     }
+
+    private func unarchiveZip(at archiveURL: URL) throws -> URL {
+
+        let enclosingFolderURL = archiveURL.deletingLastPathComponent()
+
+        let unarchiveTask = Process()
+        unarchiveTask.executableURL = URL(fileURLWithPath: "/usr/bin/unzip")
+        unarchiveTask.arguments = [archiveURL.path, "-d", enclosingFolderURL.path]
+
+        do {
+            try unarchiveTask.run()
+            unarchiveTask.waitUntilExit()
+        } catch {
+            throw UpdateInstallerError.genericUnzipError
+        }
+
+        var unzippepAppsPaths: [String]
+        do {
+            let contentOfZipPaths = try fileManager.contentsOfDirectory(atPath: enclosingFolderURL.path)
+            unzippepAppsPaths = contentOfZipPaths.filter( {$0.hasSuffix(".app") })
+        } catch {
+            throw UpdateInstallerError.unzippedContentNotFound
+        }
+
+        guard unzippepAppsPaths.count == 1, let app = unzippepAppsPaths.first else { throw UpdateInstallerError.archiveContentNotCoherent }
+
+        return enclosingFolderURL.appendingPathComponent(app)
+    }
+
+    private func unquarantineApp(at url: URL) throws {
+
+        let unquarantineTask = Process()
+        unquarantineTask.executableURL = URL(fileURLWithPath: "/usr/bin/xattr")
+        unquarantineTask.arguments = ["-d", "-r", "com.apple.quarantine", url.path]
+
+        do {
+            try unquarantineTask.run()
+            unquarantineTask.waitUntilExit()
+        } catch {
+            throw UpdateInstallerError.failedToUnquarantine
+        }
+    }
+
 }
