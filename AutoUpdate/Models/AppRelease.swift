36fe3f77e6002251c2a6bb1440e916b2f0ff5971
Ludovic Ollagnier <ludovic@ollagnier.me>
2021-05-07 17:33:03 +0200

    [feature] Added new data to mock + made public

--- AutoUpdate/Models/AppRelease.swift
+++ AutoUpdate/Models/AppRelease.swift
@@ -15,7 +15,7 @@ public struct AppRelease: Codable {
     let publicationDate: Date
     let downloadURL: URL
 
-    static func demoJSON() -> Data {
+    static public func demoJSON() -> Data {
 
         let v0_1DateComponents = DateComponents(year: 2020, month: 11, day: 20, hour: 17, minute: 45, second: 00)
         let v0_1 = AppRelease(versionName: "Version 0.1", version: "0.1", releaseNotes: "This is release notes from Beam 0.1", publicationDate: Calendar.current.date(from: v0_1DateComponents)!, downloadURL: URL(string: "https://www.beamapp.co/downloads/someZipv0.1.zip")!)
@@ -26,7 +26,26 @@ public struct AppRelease: Codable {
         let v1_1DateComponents = DateComponents(year: 2021, month: 5, day: 3, hour: 14, minute: 35, second: 00)
         let v1_1 = AppRelease(versionName: "Version 1.1", version: "1.1", releaseNotes: "This is release notes from Beam 1.1", publicationDate: Calendar.current.date(from: v1_1DateComponents)!, downloadURL: URL(string: "https://github.com/eLud/update-proto/raw/main/BeamUpdaterProto_v1.1.zip")!)
 
-        let versions = [v0_1, v0_1_1, v1_1]
+        let notes = """
+            • Pharetra, malesuada tellus amet orci iaculis et. In nunc, augue in orci netus maecenas. In eget arcu a augue. Dui pulvinar pellentesque.
+
+            • Tempor sit erat amet parturient pretium nunc.
+
+            • Urna arcu libero, neque, placerat risus porta commodo, nulla. Diam ac aliquam velit ipsum.
+
+            • Et nulla sed justo facilisi. Lobortis ligula a nisl.
+
+            • Nunc, morbi praesent non suscipit. In massa purus quis molestie. Nam lectus massa mattis fringilla quam. Vel tortor quis a sit tellus lorem amet placerat tellus. Semper dui massa phasellus nisl.
+
+            • At amet nibh nibh nibh elementum. In sagittis consectetur ut massa pulvinar.
+            """
+        let v2_0 = AppRelease(versionName: "Beam 2.0: Collaborate on Cards",
+                                            version: "2.0",
+                                            releaseNotes: notes,
+                                            publicationDate: Date(),
+                                            downloadURL: URL(string: "http://")!)
+
+        let versions = [v0_1, v0_1_1, v1_1, v2_0]
 
         let jsonEncoder = JSONEncoder()
         let data = try? jsonEncoder.encode(versions)
